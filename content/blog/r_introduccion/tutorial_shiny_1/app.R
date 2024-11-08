30e9967f8bf051728cf71ff7936f2de18c1545b9
bastianolea <bastianolea@gmail.com>
2024-11-08 16:36:18 -0300

    tutorial shiny

--- content/blog/r_introduccion/tutorial_shiny_1/app.R
+++ content/blog/r_introduccion/tutorial_shiny_1/app.R
@@ -2,15 +2,37 @@ library(shiny)
 library(bslib)
 
 ui <- page_fluid(
-  titlePanel("Mi aplicación"),
-  
   h1("Título"),
   
-  card(
-    h1("Hola"),
-    selectInput("selector",
-                label = "Elegir palabra",
-                choices = c("hola", "como", "estás")
-                )
-  )
+  h2("Introducción"),
+  p("Bienvenidx a mi primera app"),
+  
+  textInput("nombre", 
+            label = "Escribe tu nombre"),
+  
+  selectInput("saludo",
+              label = "Elije un saludo",
+              choices = c("Hola", 
+                          "Chao", 
+                          "Hasta nunca" = "Hasta nunca,")),
+  hr(),
+  
+  # salida desde server
+  textOutput("texto") 
 )
+
+server <- function(input, output, session) {
+  
+  # crear un objeto reactivo
+  texto <- reactive({
+    # paste("Hola", input$nombre) # pegar el contenido del input con otro texto
+    paste(input$saludo, input$nombre) # pegar los dos inputs
+  })
+  
+  # output del objeto reactivo
+  output$texto <- renderText({
+    texto()
+  })
+}
+
+shinyApp(ui, server)
